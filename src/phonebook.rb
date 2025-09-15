5257bfe59119324eb94d0fd97eff0299c3b2b1fd
Shane Ruegg <shaneruegg@icloud.com>
2025-09-15 14:23:13 -0500

    Implement phonebook.rb data structure, and add() functionality

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,9 +1,18 @@
 class PhoneBook
     def initialize
+        @hashForNames = {}
+	@hashForListedNumbers = {}
     end
 
     def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+        if (!number.match?(/^\d{3}-\d{3}-\d{4}$/)) then return false end
+	if (@hashForNames[name]) then return false end
+	if (@hashForListedNumbers[number] && is_listed) then return false end
+	@hashForNames[name] = { number: number, is_listed: is_listed }
+	if (is_listed) then
+	    @hashForListedNumbers[number] = name
+        end
+	true
     end
 
     def lookup(name)
