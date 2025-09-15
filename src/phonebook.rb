b880b2e751371c16b3747e243c477be4262b9e99
Shane Ruegg <shaneruegg@icloud.com>
2025-09-15 17:49:36 -0500

    Implement lookup() functionality, and fix spacing issues

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,22 +1,28 @@
 class PhoneBook
     def initialize
-        @hashForNames = {}
+	@hashForNames = {}
 	@hashForListedNumbers = {}
     end
 
     def add(name, number, is_listed)
-        if (!number.match?(/^\d{3}-\d{3}-\d{4}$/)) then return false end
+	if (!number.match?(/^\d{3}-\d{3}-\d{4}$/)) then return false end
 	if (@hashForNames[name]) then return false end
 	if (@hashForListedNumbers[number] && is_listed) then return false end
 	@hashForNames[name] = { number: number, is_listed: is_listed }
 	if (is_listed) then
-	    @hashForListedNumbers[number] = name
-        end
+		@hashForListedNumbers[number] = name
+	end
 	true
     end
 
     def lookup(name)
-        raise Exception, "Not implemented"
+	entry = @hashForNames[name]
+
+	if (entry && entry[:is_listed]) then
+		return entry[:number]
+	else
+		return nil
+	end
     end
 
     def lookupByNum(number)
